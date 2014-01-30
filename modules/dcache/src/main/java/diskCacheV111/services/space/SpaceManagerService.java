1cb9709dc4ab50b7350c2e9c819890c4b3346e77
Gerd Behrmann <behrmann@gmail.com>
2014-01-30 18:58:17 +0100

    Revert "spacemanager: Fix unlock of allocated files on delete"
    This reverts commit 8e8340a1a325dd0bc5f7d7e40faa3af9b0a64763.

--- modules/dcache/src/main/java/diskCacheV111/services/space/SpaceManagerService.java
+++ modules/dcache/src/main/java/diskCacheV111/services/space/SpaceManagerService.java
@@ -1167,7 +1167,7 @@ public final class SpaceManagerService
                 } else if (f.getState() == FileState.STORED || f.getPath() == null) {
                     f.setDeleted(true);
                     db.updateFile(f);
-                } else if (f.getState() == FileState.TRANSFERRING || f.getState() == FileState.ALLOCATED) {
+                } else if (f.getState() == FileState.TRANSFERRING) {
                     f.setPnfsId(null);
                     f.setState(FileState.ALLOCATED);
                     db.updateFile(f);
